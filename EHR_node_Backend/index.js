5c7792ba3985eaf297c30ed8170839c3e81551b4
Omkar Sonawane <130058150+Omkar-Sonawane-23@users.noreply.github.com>
2025-03-01 18:54:49 +0000

    75 % Work Lamost Completed

--- EHR_node_Backend/index.js
+++ EHR_node_Backend/index.js
@@ -46,6 +46,7 @@ dataFiles.forEach(file => {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+
 // Middleware
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
